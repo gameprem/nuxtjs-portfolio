2a7298c19be93d561bbe060dac53df4c06c46188
Nitivat Vorasan <55049986+gameprem@users.noreply.github.com>
2023-11-08 21:33:51 +0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,4 +1,6 @@
 <template>
+  <div>
+
   <div class="p-4 flex gap-4">
     <button class="btn">Nitivat Vorasan</button>
     <select class="select w-full max-w-xs" v-model="colorMode.preference">
@@ -6,14 +8,17 @@
       <option v-for="theme of themes" :key="theme">{{ theme }}</option>
     </select>
   </div>
-  <div class="hero bg-base-200 py-10">
-    <div class="hero-content text-center">
-      <div class="max-w-md">
-        <h1 class="text-5xl font-bold">Hello there สวัสดี</h1>
-        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-        <button class="btn btn-primary">Get Started</button>
-      </div>
+
+  <div class="hero min-h-screen" style="background-image: url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);">
+  <div class="hero-overlay bg-opacity-60"></div>
+  <div class="hero-content text-center text-neutral-content">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+      <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button class="btn btn-primary">Get Started</button>
     </div>
+  </div>
+
   </div>
   <div class="p-10">
     <div class="card w-96 bg-base-100 shadow-xl">
@@ -27,6 +32,8 @@
       </div>
     </div>
   </div>
+</div>
+
 </template>
 
 <script setup>
